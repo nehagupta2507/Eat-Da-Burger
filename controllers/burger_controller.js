85186bed5aec25924e1172262e4f9ff3943f72dc
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-09-09 23:35:24 -0400

    handlebar render issue fixed

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,25 +5,21 @@ const burgers = require("../models/burger");
 router.get("/", function(req, res){
 	burgers.selectAll(function(data){
 		let hbsObject = {
-            burger: data
+            burger_data: data
         };
-        console.log("hbsObj");
-        console.log(hbsObject);
 		res.render('index', hbsObject);
 	});
 });
 
 router.post("/api/burgers", function(req, res){
 	burgers.create(req.body.burger_name, function(data){
-		console.log(data);
-		res.json(data);	
+		res.redirect("/");
 	});
 });
 
 router.put('/api/burgers/:id', function(req, res){
 	var condition = "id = " + req.params.id;
 	console.log("condition ", condition);
-
 	burgers.update({"devoured": req.body.devoured}, condition, function(data){
 		res.redirect("/");
 	});
